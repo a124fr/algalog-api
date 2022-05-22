ba7d5eb2618cf2904758f5b7b89da8945f975e28
Francisco de Assis <a124fr@gmail.com>
2022-05-22 20:46:52 -0300

    Adicionado dependecia devtools

--- src/main/java/com/algaworks/algalog/api/controler/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controler/ClienteController.java
@@ -15,7 +15,7 @@ public class ClienteController {
 	public List<Cliente> listar() {
 		Cliente cliente1 = new Cliente();
 		cliente1.setId(1L);
-		cliente1.setNome("João");		
+		cliente1.setNome("João2");		
 		cliente1.setEmail("joaodascouves@algworks.com");
 		cliente1.setTelefone("34 99999-1111");
 		
