43e8e6a25c2132e316fabeedd938a4c54459b0fb
Francisco de Assis <a124fr@gmail.com>
2022-05-23 01:33:30 -0300

    Usanso bean validation e Interceptando erros e personalizando

---
+++ src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.algaworks.algalog.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import lombok.AllArgsConstructor;
+
+@ControllerAdvice
+@AllArgsConstructor
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	private MessageSource messageSource;
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<MensagemDeErro.Campo> campos = new ArrayList<>();
+		
+		for(ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String nome = ((FieldError)error).getField();
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());			
+			campos.add(new MensagemDeErro.Campo(nome, mensagem));
+		}
+		
+		MensagemDeErro erros = new MensagemDeErro();
+		erros.setStatus(status.value());
+		erros.setDataHora(LocalDateTime.now());
+		erros.setTitulo("Um ou mais campos estão inválidos. Faça o prenchimento e tente novamente");
+		erros.setCampos(campos);		
+		
+		return handleExceptionInternal(ex, erros, headers, status, request);
+	}
+}
