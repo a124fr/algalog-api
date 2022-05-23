6c175f6eafb67c3738384140be33176d472541f3
Francisco de Assis <a124fr@gmail.com>
2022-05-22 22:25:29 -0300

    Usando injeção de dependencia com construtor, e usando lombok para gerar o construtor com as propriedades da classe

--- src/main/java/com/algaworks/algalog/api/controler/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controler/ClienteController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor // anotação vai gerar um construtor com todas as proprieddes que temos na nossa classe
 @RestController
 public class ClienteController {
-		
-	@Autowired
-	private ClienteRepository clienteRepository;
 	
+	private ClienteRepository clienteRepository;
+			
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {			
 		return clienteRepository.findAll();
