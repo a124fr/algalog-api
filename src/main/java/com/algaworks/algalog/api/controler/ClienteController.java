3a0edbe3b57549d065915fb9884565a07f407755
Francisco de Assis <a124fr@gmail.com>
2022-05-22 22:09:36 -0300

    Usando o JPA  e mapeando a tabela cliente. E fazendo a consulta co entitymanager.

--- src/main/java/com/algaworks/algalog/api/controler/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controler/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.api.controler;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +14,13 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
-	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João2");		
-		cliente1.setEmail("joaodascouves@algworks.com");
-		cliente1.setTelefone("34 99999-1111");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setEmail("mariadasilva@algworks.com");
-		cliente2.setTelefone("11 97777-2222");
-		
-		return Arrays.asList(cliente1, cliente2);
+	public List<Cliente> listar() {			
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 	
 }
